e317206f48c82abc96333e28a3c22ea395c4d139
Arturo Ghinassi <arturo.ghinassi@uniroma1.it>
2024-01-08 10:13:18 +0100

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -3,6 +3,4 @@
 real_script_path="$(realpath "$0")"
 app_home="$(realpath "$(dirname "$real_script_path")")"
 
-echo "$app_home"
-
 docker-compose -f "$app_home/../docker-compose.yml" up --remove-orphans -V --build -d
